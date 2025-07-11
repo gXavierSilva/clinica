4aa1baca5e29bc86e316107a45d89ab58cb5e75b
gXavierSilva <xaviergabriel856@gmail.com>
2025-07-11 16:36:36 -0300

    system structure and API dev

---
+++ models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+class Usuario(Base):
+    __tablename__ = "usuarios"
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
